436c4ab94ba25d4e84c05df5e7f2430880a17fcf
smanihwr <smanihwr@gmail.com>
2018-02-15 19:53:48 -0600

    initial checkin

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
+	public String sayHello(@PathVariable("name") String name) {
+		return "Hello " + name;
+	}
+
+}
